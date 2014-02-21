a13eb5a768a9400b0059f39b8398a610ce0588e6
SomaticIT <contact@somatic.fr>
2014-02-21 17:46:33 +0100

    Add a all module which load every handlers

---
+++ src/all.ts
@@ -0,0 +1,5 @@
+﻿/// <amd-dependency path="./text" />
+/// <amd-dependency path="./jquery" />
+/// <amd-dependency path="./ui" />
+/// <amd-dependency path="./filesize" />
+/// <amd-dependency path="./development" />
