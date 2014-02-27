8ad90ea6b2f50783c4db7033d3e9d999b7ff473c
SomaticIT <contact@somatic.fr>
2014-02-27 23:52:43 +0100

    Update build distribution

--- dist/all.js
+++ dist/all.js
@@ -1,1 +1,5 @@
-﻿
+﻿define(["require", "exports", "./development", "./filesize", "./helpers", "./jquery", "./text", "./ui"], function(require, exports) {
+    var result = true;
+    
+    return result;
+});
