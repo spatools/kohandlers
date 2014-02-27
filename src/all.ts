fafc95c4b6dc24661038b806c942a70a3c34336d
SomaticIT <contact@somatic.fr>
2014-02-27 23:52:28 +0100

    Fix all loader module

--- src/all.ts
+++ src/all.ts
@@ -1,5 +1,9 @@
-﻿/// <amd-dependency path="./text" />
-/// <amd-dependency path="./jquery" />
-/// <amd-dependency path="./ui" />
+﻿/// <amd-dependency path="./development" />
 /// <amd-dependency path="./filesize" />
-/// <amd-dependency path="./development" />
+/// <amd-dependency path="./helpers" />
+/// <amd-dependency path="./jquery" />
+/// <amd-dependency path="./text" />
+/// <amd-dependency path="./ui" />
+
+var result = true;
+export = result;
